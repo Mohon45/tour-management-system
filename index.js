8eb904411148a8b50d78091317d7e9f1c40f83d0
Md: Mohon <mdmohon6145@gmail.com>
2022-09-20 19:11:29 +0600

    atlas added

--- index.js
+++ index.js
@@ -5,12 +5,18 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const tourRoute = require("./routes/tour.route");
 
-// database connection
-mongoose.connect(process.env.DATABASE_LOCAL_URI).then(() => {
-  console.log("Database connection is successful");
-});
+// database connections
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m0coh.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+// Mongoose Connect
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Database connection successful"))
+  .catch((err) => console.log(err));
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5002;
 
 app.use(cors());
 app.use(express.json());
